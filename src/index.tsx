a3f1f11675129579f792aa8c767d2487493da7a7
Paul Johnson <paul@johnson.kiwi.nz>
2020-03-28 17:47:28 -0700

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { FetchExample } from "./FetchExample";
+import './styles.css'; 
 
 const fetchElement = document.getElementById("fetch_example");
 render(<FetchExample />, fetchElement);
